24a3a0209c755db3be446420719fd1a3d5b0bf27
letItCurl <rolandlopez.developer@gmail.com>
2020-09-08 12:51:56 +0200

    ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+    //public name: string;
+    //public amount: string;
+    // can be declared directly into constructor
+    constructor(public name: string, public amount: string){}
+}
