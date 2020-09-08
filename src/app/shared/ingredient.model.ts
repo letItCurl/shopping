74e2866b61af64937d06e7ddae359b8f2d03b1bc
letItCurl <rolandlopez.developer@gmail.com>
2020-09-08 12:56:22 +0200

    shopping list *ngFor

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -2,5 +2,5 @@ export class Ingredient {
     //public name: string;
     //public amount: string;
     // can be declared directly into constructor
-    constructor(public name: string, public amount: string){}
+    constructor(public name: string, public amount: number){}
 }
