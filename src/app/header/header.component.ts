4bb63ebecf02c1e30e42c32c1b873a27cd80d6cb
letItCurl <rolandlopez.developer@gmail.com>
2020-09-15 14:04:42 +0200

    workin navbar links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+    @Output() featureSelected = new EventEmitter<string>();
+    
     constructor() { }
 
     ngOnInit() { }
+
+    onSelect(link: string): void{
+        this.featureSelected.emit(link);
+    }
 }
