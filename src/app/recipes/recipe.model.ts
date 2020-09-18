740612a17db797efc6b119cde54803ed8fa9dc4d
letItCurl <rolandlopez.developer@gmail.com>
2020-09-18 09:30:08 +0200

    adding ingredients to recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
     public id: number;
+    public ingredients: Ingredient[];
 
-    constructor(id: number, name: string, description: string, imagePath: string){
+    constructor(id: number, name: string, description: string, imagePath: string, ingredients: Ingredient[]){
         this.id = id;
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 }
